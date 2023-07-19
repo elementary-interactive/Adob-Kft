e947bd59a22188785310c95b2b896d428808c512
Balazs Ercsey <laze@laze.hu>
2023-07-19 09:31:58 +0200

    Vite fix.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/splide.min.js', 'resources/js/site.js'],
