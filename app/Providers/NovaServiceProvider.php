305a011621b0c8f0131b527dd0109a68b660cd0b
Balazs Ercsey <laze@laze.hu>
2023-07-19 10:36:54 +0200

    Enable all admins on the Gate.

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -127,9 +127,10 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
   protected function gate()
   {
     Gate::define('viewNova', function ($user) {
-      return in_array($user->email, [
-        //
-      ]);
+      return true;
+      // return in_array($user->email, [
+      //   //
+      // ]);
     });
   }
 
