6c128956f36d3642acbacc549c59b337b094f13f
Balazs Ercsey <laze@laze.hu>
2023-07-21 11:13:51 +0200

    Force https.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-    server: {
-        https: true,
+    build: {
+        server: {
+            https: true,
+        },
     },
     plugins: [
         laravel({
