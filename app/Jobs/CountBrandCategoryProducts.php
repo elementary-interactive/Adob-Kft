c1166a7e6459ef21135c2d33e2dbd60495481036
Balazs Ercsey <laze@laze.hu>
2023-08-16 11:36:14 +0200

    Fix job.

--- app/Jobs/CountBrandCategoryProducts.php
+++ app/Jobs/CountBrandCategoryProducts.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Neon\Models\Statuses\BasicStatus;
 
 class CountBrandCategoryProducts implements ShouldQueue
 {
