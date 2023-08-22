e14f4e656bc0193d7bc7652d14a041d1746b1be6
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2023-08-22 17:56:45 +0200

    Code clean up.

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -5,6 +5,8 @@ namespace App\Imports;
 use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Columns;
+use App\Models\ProductImport;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithValidation;
@@ -14,6 +16,7 @@ use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Neon\Admin\Models\Admin;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 use Illuminate\Validation\Rule;
 use Laravel\Nova\Notifications\NovaNotification;
@@ -21,7 +24,8 @@ use Laravel\Nova\Notifications\NovaChannel;
 use Laravel\Nova\URL;
 use Neon\Models\Statuses\BasicStatus;
 
-class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChunkReading, ShouldQueue
+
+class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChunkReading// ShouldQueue
 {
   use Importable;
 
@@ -29,35 +33,42 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
 
   const MAX_SUB_CATEGORY_COUNT    = 5;
 
-  const COLUMN_WEB                = 'web';
-  const COLUMN_PRODUCT_ID         = 'cikkszam';
-  const COLUMN_PRODUCT_NAME       = 'megnevezes';
-  const COLUMN_BRAND              = 'marka';
-  const COLUMN_PRICE              = 'webar';
-  const COLUMN_DESCRIPTION        = 'leiras';
-  const COLUMN_DESCRIPTION_UPDATE = 'webleir';
-  const COLUMN_PACKAGING          = 'csomagolas';
-  const COLUMN_EAN                = 'ean';
-  const COLUMN_PRODUCT_NUMBER     = 'termekszam';
-  const COLUMN_ON_SALE            = 'akcios';
-  const COLUMN_MAIN_CATEGORY      = 'main_kat';
-  const COLUMN_COMMAND            = 'web';
-  const COLUMN_SUB_CATEGORY       = 'alkat';
-  const COLUMN_DESC_TO_CATEGORY   = 'webkatleir';
+  // const COLUMN_COMMAND->value                = 'COMMAND->value';
+  // const COLUMN_PRODUCT_ID->value         = 'cikkszam';
+  // const COLUMN_PRODUCT_NAME->value       = 'megnevezes';
+  // const COLUMN_BRAND->value              = 'marka';
+  // const COLUMN_PRICE              = 'COMMAND->valuear';
+  // const COLUMN_DESCRIPTION->value        = 'leiras';
+  // const COLUMN_DESCRIPTION_UPDATE->value = 'COMMAND->valueleir';
+  // const COLUMN_PACKAGING->value          = 'csomagolas';
+  // const COLUMN_EAN                = 'ean';
+  // const COLUMN_PRODUCT_NUMBER->value     = 'termekszam';
+  // const COLUMN_ON_SALE->value            = 'akcios';
+  // const COLUMN_MAIN_CATEGORY->value      = 'main_kat';
+  // const COLUMN_COMMAND            = 'COMMAND->value';
+  // const COLUMN_SUB_CATEGORY->value       = 'alkat';
+  // const COLUMN_DESCRIPTION_TO_CATEGORY->value   = 'COMMAND->valuekatleir';
 
-  public $importedBy;
+  static $columns = \App\Models\Columns::class;
+
+  public $imported_by;
 
   private $headerRow;
   private $headerErrors = [];
 
-  private $aggregateProduct = "webagrp";
+  private $aggregateProduct = "COMMAND->valueagrp";
   private $agrIdPostfix = "-AGR";
-  private $track;
+
   private $category_group_count = 0;
 
-  public function __construct(Admin $importedBy)
+  private $tracker;
+
+  public function __construct(Admin $imported_by)
   {
-    $this->importedBy = $importedBy;
+    $this->imported_by  = $imported_by;
+    $this->tracker      = new ProductImport([
+      'imported_by' => $imported_by
+    ]);
   }
 
   public function model(array $row)
@@ -67,11 +78,11 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
      * @todo: Header based model validation.
      */
 
-    // if (!array_key_exists('web', $row)) {
-    //   $this->error('Nem található \'web\' oszlop!');
+    // if (!array_key_exists('COMMAND->value', $row)) {
+    //   $this->error('Nem található \'COMMAND->value\' oszlop!');
     // }
     // dd($row);
-    // dd($this->importedBy);
+    // dd($this->imported_by);
 
     // return new Product([
     //   'name'  => $row['name'],
@@ -80,16 +91,12 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
 
     $isAggregateProduct = false;
 
-    if ($row[self::COLUMN_WEB] == 'y' || $row[self::COLUMN_WEB] == 'Y') {
-      $aggregateId = $this->saveProduct($row, $isAggregateProduct, true);
+    if (self::toSave($row)) {
+      $aggregateId = $this->saveProduct($row, $isAggregateProduct, self::isActive($row));
     }
 
-    if ($row[self::COLUMN_WEB] == 'i' || $row[self::COLUMN_WEB] == 'I') {
-      $aggregateId = $this->saveProduct($row, $isAggregateProduct, false);
-    }
-
-    if ($row[self::COLUMN_WEB] == 'd' || $row[self::COLUMN_WEB] == 'D') {
-      // $this->deleteProduct($row, $isAggregateProduct);
+    if (self::toDelete($row)) {
+      $this->deleteProduct($row, $isAggregateProduct);
     }
   }
 
@@ -99,13 +106,26 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
   public function rules(): array
   {
     return [
-      self::COLUMN_WEB => 'required',
+      self::$columns::PRODUCT_ID->value         => 'required',
+      self::$columns::PRODUCT_NAME->value       => 'required',
+      self::$columns::BRAND->value              => '',
+      self::$columns::PRICE->value              => '',
+      self::$columns::DESCRIPTION->value        => '',
+      self::$columns::DESCRIPTION_UPDATE->value => '',
+      self::$columns::PACKAGING->value          => '',
+      self::$columns::EAN->value                => 'numeric',
+      self::$columns::PRODUCT_NUMBER->value     => '',
+      self::$columns::ON_SALE->value            => '',
+      self::$columns::MAIN_CATEGORY->value      => 'required',
+      self::$columns::COMMAND->value            => '',
+      self::$columns::SUB_CATEGORY->value       => '',
+      self::$columns::DESCRIPTION_TO_CATEGORY->value   => '',
     ];
   }
 
   private function error(string $message, string $icon = 'exclamation')
   {
-    $this->importedBy->notify(
+    $this->imported_by->notify(
       NovaNotification::make()
         ->message($message)
         // ->action('Download', URL::remote('https://example.com/report.pdf'))
@@ -134,68 +154,128 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
   {
     $is_new = null;
 
-    $pid = $isAggregate ? $this->generateAgrProductID($row[self::COLUMN_PRODUCT_ID], $this->agrIdPostfix) : $row[self::COLUMN_PRODUCT_ID];
+    $pid = $isAggregate ? $this->generateAgrProductID($row[self::$columns::PRODUCT_ID->value], $this->agrIdPostfix) : $row[self::$columns::PRODUCT_ID->value];
 
     $product = Product::firstOrNew(['product_id' => $pid]);
-    $product->name            = $row[self::COLUMN_PRODUCT_NAME];
-    $product->slug            = Str::slug($row[self::COLUMN_PRODUCT_NAME], '-');
-    $product->packaging       = array_key_exists(self::COLUMN_PACKAGING, $row) ? $row[self::COLUMN_PACKAGING] : null;
+    $product->name            = $row[self::$columns::PRODUCT_NAME->value];
+    $product->slug            = Str::slug($row[self::$columns::PRODUCT_NAME->value], '-');
+    $product->packaging       = array_key_exists(self::$columns::PACKAGING->value, $row) ? $row[self::$columns::PACKAGING->value] : null;
     $product->description     = (
-      ($row[self::COLUMN_DESCRIPTION_UPDATE] == "y" || $row[self::COLUMN_DESCRIPTION_UPDATE] == "Y") &&
-      array_key_exists(self::COLUMN_DESCRIPTION, $row)
-    ) ? $row[self::COLUMN_DESCRIPTION] : null;
-    $product->ean             = array_key_exists(self::COLUMN_EAN, $row) ? (is_numeric($row[self::COLUMN_EAN]) ? $row[self::COLUMN_EAN] : null) : null;
-    $product->product_number  = array_key_exists(self::COLUMN_PRODUCT_NUMBER, $row) ? $row[self::COLUMN_PRODUCT_NUMBER] : null;
-    $product->price           = $row[self::COLUMN_PRICE];
-    $product->on_sale         = (array_key_exists(self::COLUMN_ON_SALE, $row) && ($row[self::COLUMN_ON_SALE] == "y" || $row[self::COLUMN_ON_SALE] == "Y"));
+      (strtolower($row[self::$columns::DESCRIPTION_UPDATE->value]) === 'y') &&
+      array_key_exists(self::$columns::DESCRIPTION->value, $row)
+    ) ? $row[self::$columns::DESCRIPTION->value] : null;
+    $product->ean             = array_key_exists(self::$columns::EAN->value, $row) ? (is_numeric($row[self::$columns::EAN->value]) ? $row[self::$columns::EAN->value] : null) : null;
+    $product->product_number  = array_key_exists(self::$columns::PRODUCT_NUMBER->value, $row) ? $row[self::$columns::PRODUCT_NUMBER->value] : null;
+    $product->price           = $row[self::$columns::PRICE->value];
+    $product->on_sale         = (array_key_exists(self::$columns::ON_SALE->value, $row) && ($row[self::$columns::ON_SALE->value] == "y" || $row[self::$columns::ON_SALE->value] == "Y"));
     $product->status          = ($is_active) ? BasicStatus::Active->value : BasicStatus::Inactive->value;
 
     /** 
      * @var Brand $brand The product's brand.
      */
     $brand = Brand::firstOrCreate([
-      'name'  => $row[self::COLUMN_BRAND]
+      'name'  => $row[self::$columns::BRAND->value]
     ], [ //- Fill up data.
-      'slug'        => Str::slug($row[self::COLUMN_BRAND]),
+      'slug'        => Str::slug($row[self::$columns::BRAND->value]),
       'is_featured' => false
     ]);
+    // Connect brand to product.
     $product->brand()->associate($brand);
 
     if ($product->exists) {
-      // $this->track->increaseModified();
+      $this->tracker->increaseProductModified();
       $is_new = false;
     } else {
       $is_new = true;
-      // $this->track->increaseInserted();
+      $this->tracker->increaseProductInserted();
     }
 
     $product->save();
 
     /** Check is there category & adding to categories
      */
-    $categories = $this->extract_categories($row);
+    $categories = $this->XXXXXXX_categories($row);
 
     // leírások feltöltése a kategóriákhoz
     $saveDescToCategory = null;
     $category_description = null;
 
-    if (array_key_exists(self::COLUMN_DESC_TO_CATEGORY, $row)) {
+    if (array_key_exists(self::$columns::DESCRIPTION_TO_CATEGORY->value, $row)) {
 
-      $saveDescToCategory = $this->calculate_target_category_index($row[self::COLUMN_DESC_TO_CATEGORY]);
-      if (array_key_exists(self::COLUMN_DESCRIPTION, $row)) {
-        $category_description = $row[self::COLUMN_DESCRIPTION];
+      $saveDescToCategory = $this->calculate_target_category_index($row[self::$columns::DESCRIPTION_TO_CATEGORY->value]);
+      if (array_key_exists(self::$columns::DESCRIPTION->value, $row)) {
+        $category_description = $row[self::$columns::DESCRIPTION->value];
       }
     }
 
+    dd($product, $categories);
     /** Upload categories...
      */
     if (!$is_new)
-    {
+    { //- If modifying product we detach from all categories.
       $product->categories()->detach();
     }
-    $this->attach_product_to_categories($product, $categories, $saveDescToCategory, $category_description);
+
+    foreach ($categories as $cc)
+    {
+      $product->categories()->attach($cc);
+    }
+    // $this->attach_product_to_categories($product, $categories, $saveDescToCategory, $category_description);
 
     return $product;
+  }
+
+  /** Parse and save categories. Returns with nodes where to product should be
+   * attached.
+   * 
+   * @param array $row The row data.
+   * 
+   * @return array $categories
+   */
+  private function XXXXXXX_categories(array $row): array
+  {
+    $columns  = array_keys($row);
+
+    $result   = [];
+
+    for ($categories_index = 1; $categories_index <= 3; $categories_index++)
+    {
+      $main_category = Category::firstOrCreate([
+        'slug'        => Str::slug($row[self::$columns::MAIN_CATEGORY->value]),
+        'parent_id'   => null
+      ], [
+        'name'        => $row[self::$columns::MAIN_CATEGORY->value],
+        'description' => $row[self::$columns::DESCRIPTION->value]
+      ]);
+
+      $category = null;
+      
+      for ($sub_category_count = 1; $sub_category_count <= self::MAX_SUB_CATEGORY_COUNT; $sub_category_count++)
+      {
+        if (is_null($category))
+        {
+          $category = $main_category;
+        }
+        $sub_category_column = Arr::first(preg_grep(($categories_index > 1) ? "/alkat{$sub_category_count}[^\d]*{$categories_index}[^\w]*/" : "/alkat{$sub_category_count}/", $columns));
+        
+        if (isset($row[$sub_category_column]) && !is_null($row[$sub_category_column]))
+        {
+          $sub_category = Category::firstOrCreate([
+            'slug'        => Str::slug($row[$sub_category_column]),
+            'parent_id'   => $category->id
+          ], [
+            'name'        => $row[$sub_category_column]
+          ]);
+          $sub_category->save();
+          $sub_category->makeChildOf($category);
+          
+          $category = $sub_category;
+        }
+      }
+      $result[$category->id] = $category;
+    }
+
+    return $result;
   }
 
   private function generateAgrProductID($col, $postfix)
@@ -215,12 +295,14 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
   {
     $categories = [];
 
-    if (array_key_exists(self::COLUMN_MAIN_CATEGORY, $row)) {
-      $base_category_group[] = $row[self::COLUMN_MAIN_CATEGORY];
+     
+
+    if (array_key_exists(self::$columns::MAIN_CATEGORY, $row)) {
+      $base_category_group[] = $row[self::$columns::MAIN_CATEGORY->value];
 
       for ($i = 1; $i <= self::MAX_SUB_CATEGORY_COUNT; $i++) {
-        if (array_key_exists(self::COLUMN_SUB_CATEGORY . $i, $row)) {
-          $base_category_group[] = $row[self::COLUMN_SUB_CATEGORY . $i];
+        if (array_key_exists(self::$columns::SUB_CATEGORY->value . $i, $row)) {
+          $base_category_group[] = $row[self::$columns::SUB_CATEGORY->value . $i];
         } else {
           break;
         }
@@ -233,12 +315,12 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
       $categoryGroup = [];
 
       //pushing main_kat
-      if (array_key_exists(self::COLUMN_MAIN_CATEGORY . "_" . $i, $row)) {
-        $categoryGroup[] = $row[self::COLUMN_MAIN_CATEGORY . "_" . $i];
+      if (array_key_exists(self::$columns::MAIN_CATEGORY->value . "_" . $i, $row)) {
+        $categoryGroup[] = $row[self::$columns::MAIN_CATEGORY->value . "_" . $i];
 
         for ($j = 1; $j <= self::MAX_SUB_CATEGORY_COUNT; $j++) {
-          if (array_key_exists(self::COLUMN_SUB_CATEGORY . $j . "_" . $i, $row)) {
-            $categoryGroup[] = $row[self::COLUMN_SUB_CATEGORY . $j . "_" . $i];
+          if (array_key_exists(self::$columns::SUB_CATEGORY->value . $j . "_" . $i, $row)) {
+            $categoryGroup[] = $row[self::$columns::SUB_CATEGORY->value . $j . "_" . $i];
           } else {
             break;
           }
@@ -270,7 +352,7 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
    *
    * @param $categories
    * @param null $targetCategoryIndexes
-   * @param null $catDescription
+   * @param null catDescription
    * @return array
    */
   private function save_categories($categories, $targetCategoryIndexes = null, $catDescription = null)
@@ -349,5 +431,20 @@ class ProductsImport implements ToModel, WithValidation, WithHeadingRow, WithChu
     }
 
     return false;
+  }
+
+  public static function toSave(array $row): bool
+  {
+    return (strtolower($row[self::$columns::COMMAND->value]) === 'y' || strtolower($row[self::$columns::COMMAND->value]) === 'i');
+  }
+
+  public static function isActive(array $row): bool
+  {
+    return (strtolower($row[self::$columns::COMMAND->value]) === 'y');
+  }
+
+  public static function toDelete(array $row): bool
+  {
+    return (strtolower($row[self::$columns::COMMAND->value]) === 'd');
   }
 }
---
+++ app/Models/Columns.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+enum Columns: string {
+    case PRODUCT_ID                 = 'cikkszam';
+    case PRODUCT_NAME               = 'megnevezes';
+    case BRAND                      = 'marka';
+    case PRICE                      = 'webar';
+    case DESCRIPTION                = 'leiras';
+    case DESCRIPTION_UPDATE         = 'webleir';
+    case DESCRIPTION_TO_CATEGORY    = 'webkatleir';
+    case PACKAGING                  = 'csomagolas';
+    case EAN                        = 'ean';
+    case PRODUCT_NUMBER             = 'termekszam';
+    case ON_SALE                    = 'akcios';
+    case MAIN_CATEGORY              = 'main_kat';
+    case COMMAND                    = 'web';
+    case SUB_CATEGORY               = 'alkat';
+    
+}
