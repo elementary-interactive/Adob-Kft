00495e1b0547d477edc275b519f02675aeca4476
Balazs Ercsey <laze@laze.hu>
2023-11-06 12:28:16 +0100

    May it works?

--- app/Jobs/ADOBCategoryImportJob.php
+++ app/Jobs/ADOBCategoryImportJob.php
@@ -5,7 +5,8 @@ namespace App\Jobs;
 /**
  * Whhoooo!!! Be careful!!!
  **/
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 600);
+set_time_limit(600);
 
 use App\Models\Brand;
 use App\Models\Category;
