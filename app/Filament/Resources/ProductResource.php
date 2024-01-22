4fe454c8c924e4725ceb8d47b01e081929ba94a1
Balazs Ercsey <laze@laze.hu>
2024-01-22 12:09:07 +0100

    We need like this.

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -86,9 +86,11 @@ class ProductResource extends Resource
                                 $obj = $model::find($get('id'));
 
                                 foreach ($state as $file) {
-                                    if (!is_string($file)) {
+                                    try {
                                         $obj->addMedia($file)
                                             ->toMediaCollection($model::MEDIA_COLLECTION);
+                                    } catch (\Exception $e) {
+
                                     }
                                 }
                             }),
