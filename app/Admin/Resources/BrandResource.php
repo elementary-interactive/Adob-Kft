842747a56051f76e127b5ca889b645dc9de8137d
Balazs Ercsey <laze@laze.hu>
2024-03-04 23:43:06 +0100

    All the peace of shit.

--- app/Admin/Resources/BrandResource.php
+++ app/Admin/Resources/BrandResource.php
@@ -16,6 +16,7 @@ use Filament\Tables\Table;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Str;
 use Neon\Models\Scopes\ActiveScope;
