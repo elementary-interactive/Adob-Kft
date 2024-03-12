a12241385c632525f4d72812cf1a08fb91f6fb02
Balazs Ercsey <laze@laze.hu>
2024-03-12 17:40:38 +0100

    WIP: Shit fix.

--- resources/js/components/a-d-o-b-media-handler.js
+++ resources/js/components/a-d-o-b-media-handler.js
@@ -1,4 +1,4 @@
-import * as FilePond from 'filepond'x
+import * as FilePond from 'filepond'
 import Cropper from 'cropperjs'
 import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
