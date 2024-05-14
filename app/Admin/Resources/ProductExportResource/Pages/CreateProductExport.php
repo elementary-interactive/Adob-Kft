7907dd9e61b9cdc95bce87e2a042f228d2ba069d
Balazs Ercsey <laze@laze.hu>
2024-05-14 18:45:33 +0200

    Filename fixes.

--- app/Admin/Resources/ProductExportResource/Pages/CreateProductExport.php
+++ app/Admin/Resources/ProductExportResource/Pages/CreateProductExport.php
@@ -6,7 +6,7 @@ use App\Admin\Resources\ProductExportResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreateProductImport extends CreateRecord
+class CreateProductExport extends CreateRecord
 {
     protected static string $resource = ProductExportResource::class;
 }
