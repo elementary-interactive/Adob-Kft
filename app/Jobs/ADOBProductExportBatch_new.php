fdc82464009ce5c9b14f161567f331977c95853b
Balazs Ercsey <laze@laze.hu>
2024-07-31 14:21:33 +0200

    WIP

--- app/Jobs/ADOBProductExportBatch_new.php
+++ app/Jobs/ADOBProductExportBatch_new.php
@@ -2,31 +2,18 @@
 
 namespace App\Jobs;
 
-use App\Exports\ADOBProductCollectionExport;
 use App\Exports\ADOBProductsExport_new;
-use App\Models\Columns\ADOBProductsExportColumns;
 use App\Models\ProductExport;
-use Carbon\Carbon;
-use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-
-use Illuminate\Bus\Batch;
-use Illuminate\Support\Facades\Bus;
-use Throwable;
-// use Excel;
-use Filament\Notifications\Notification;
-use Illuminate\Support\Facades\Log;
-use Laravel\Nova\Notifications\NovaNotification;
 use Logtail\Monolog\LogtailHandler;
 use Monolog\Logger;
-use Maatwebsite\Excel\Facades\Excel;
 
-class ADOBProductExportBatch_new
+class ADOBProductExportBatch_new implements ShouldQueue
 {
   use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -40,17 +27,17 @@ class ADOBProductExportBatch_new
     $this->logger->pushHandler(new LogtailHandler('1sKmnmxToqZ5NPAJy6EfvyAZ'));
   }
 
-  // /**
-  //  * Get the middleware the job should pass through.
-  //  *
-  //  * @return array<int, object>
-  //  */
-  // public function middleware(): array
-  // {
-  //   return [
-  //     new WithoutOverlapping($this->export->id)
-  //   ];
-  // }
+  /**
+   * Get the middleware the job should pass through.
+   *
+   * @return array<int, object>
+   */
+  public function middleware(): array
+  {
+    return [
+      new WithoutOverlapping($this->export->id)
+    ];
+  }
 
   /**
    * Execute the job.
