f704895e3cce62af1ab65896805cbe89158739df
Balazs Ercsey <laze@laze.hu>
2024-08-03 17:12:15 +0200

    WIP

--- app/Models/ProductImport.php
+++ app/Models/ProductImport.php
@@ -83,14 +83,14 @@ class ProductImport extends Model
     parent::boot();
 
     static::saving(function ($model) {
-      $fails = json_decode(Cache::pull('fails'));
+      $fails = json_decode(Cache::pull($model->id . '_fails'));
 
-      $model->brands_inserted = Cache::pull('brands_inserted');
-      $model->brands_modified = Cache::pull('brands_modified');
-      $model->categories_inserted = Cache::pull('categories_inserted');
-      $model->categories_modified = Cache::pull('categories_modified');
-      $model->products_inserted = Cache::pull('products_inserted');
-      $model->products_modified = Cache::pull('products_modified');
+      $model->brands_inserted = Cache::pull($model->id . '_brands_inserted');
+      $model->brands_modified = Cache::pull($model->id . '_brands_modified');
+      $model->categories_inserted = Cache::pull($model->id . '_categories_inserted');
+      $model->categories_modified = Cache::pull($model->id . '_categories_modified');
+      $model->products_inserted = Cache::pull($model->id . '_products_inserted');
+      $model->products_modified = Cache::pull($model->id . '_products_modified');
       $model->data = json_encode([
         'fails'         => $fails,
         'fails_counter' => count($fails)
