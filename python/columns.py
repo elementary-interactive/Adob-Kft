47c209a2242f52c735374bd885d3fda2859b45db
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-09-10 15:31:04 +0200

    feat: create column definitions

---
+++ python/columns.py
@@ -0,0 +1,12 @@
+column_mapping = {
+    "product_id": "cikkszam",
+    "product_name": "terméknév",
+    "brand_name": "márka",
+    "ean": "ean",
+    "price": "webár",
+    "main_category": "fő kategória",
+    "categories": "kategóriák",
+    "url": "termék link",
+    "status": "státusz (1: aktív 0:inaktív)",
+    "description": "Termék leírás"
+}
