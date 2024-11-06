6898a8d30805703a50773a88915b76f9a8ac3f0d
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-11-06 16:42:25 +0000

    feat(migrationst): simplify migrations on `image_imports` table

--- database/migrations/2024_06_19_183122_create_image_imports_table.php
+++ database/migrations/2024_06_19_183122_create_image_imports_table.php
@@ -12,35 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('image_imports', function (Blueprint $table) {
-            $table->uuid('id')
-                ->primary();
-            $table->uuid('imported_by_id')
-                ->nullable()
-                ->default(null);
-            $table->string('status')
-                ->nullable()
-                ->default(null);
-            $table->longText('images')
-                ->nullable()
-                ->default(null);
-
-            $table->longText('data')
-                ->nullable()
-                ->default(null);
-            $table->text('job')
-                ->nullable()
-                ->default(null);
-            $table->string('file')
-                ->nullable()
-                ->default(null);
-
-            $table->timestamp('finished_at')
-                ->nullable()
-                ->default(null);
-
+            $table->uuid('id')->primary();
+            $table->uuid('imported_by_id')->nullable()->default(null);
+            $table->string('status')->nullable()->default(null);
+            $table->longText('images')->nullable()->default(null);
+            $table->longText('data')->nullable()->default(null);
+            $table->text('job')->nullable()->default(null);
+            $table->string('file')->nullable()->default(null);
+            $table->integer('records_counter', false, true)->default(0);
+            $table->integer('records_handled', false, true)->default(0);
+            $table->timestamp('finished_at')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
-
             $table->foreign('imported_by_id')->references('id')->on('admins');
         });
     }
