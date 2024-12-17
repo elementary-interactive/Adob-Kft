4ba0208872e5e87008e9e5dee97e9ad28b1b6b4c
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-12-17 15:40:58 +0000

    fix export file path

--- app/Jobs/ADOBProductExportBatch_new.php
+++ app/Jobs/ADOBProductExportBatch_new.php
@@ -88,7 +88,7 @@ class ADOBProductExportBatch_new implements ShouldQueue
 
         Notification::make()
             ->title('Exportálás folyamata...')
-            ->body((($this->export->fails_counter > 0) ? 'Végeztünk.' : 'Sikeresen végeztünk!') . ' A keresett állomány itt tölthető le: <a href="' . Storage::url($this->export->file) . '">' . $this->export->file . '</a>')
+            ->body((($this->export->fails_counter > 0) ? 'Végeztünk.' : 'Sikeresen végeztünk!') . ' A keresett állomány itt tölthető le: <a href="' . Storage::url('app/exports/'.$this->export->file) . '">' . $this->export->file . '</a>')
             ->success()
             ->sendToDatabase($this->export->exported_by);
     }
