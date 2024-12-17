97949c65c79981eff506a82ec5de292df85aca3a
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-12-17 15:32:41 +0000

    remove unused import

--- app/Jobs/ADOBProductExportBatch_new.php
+++ app/Jobs/ADOBProductExportBatch_new.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Exports\ADOBProductsExport_new;
 use App\Models\ProductExport;
 use Filament\Notifications\Notification;
 use Illuminate\Bus\Queueable;
@@ -50,10 +49,10 @@ class ADOBProductExportBatch_new implements ShouldQueue
         try {
             $this->exportStarted();
 
-            $outputFile = storage_path('app/' . $this->export->file);
+            $outputFile = storage_path('app/exports/' . $this->export->file);
             $scriptPath = base_path('python/run.sh');
 
-            $process = Process::run('sh ' . $scriptPath . ' ' . $outputFile);        //$process = Process::run('pwd');
+            $process = Process::run('sh ' . $scriptPath . ' ' . $outputFile);
 
             $this->logger->info('Export process', (array)$process->output());
 
