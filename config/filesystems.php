e9758b4d5a9889342a308d1a3776b339a793a594
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-12-26 15:56:29 +0000

    feat: add entry for `spaces` for filesystem disks

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,16 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_URL'),
+        ],
+
 
     ],
 
